dd30a5fd1a1f5d4f6a086b62958e49a5ae774203
prezi1 <Patrick.Prettenhofer@edu.campus02.at>
2022-01-11 21:05:26 +0100

    char Code 46 as dot integrated in function

--- src/app/fitnesscenter-ms/entities/fitnessEquipment.ts
+++ src/app/fitnesscenter-ms/entities/fitnessEquipment.ts
@@ -18,7 +18,8 @@ export interface FitnessEquipment {
 
   export function numberOnly(event:any) {
     const charCode = (event.which) ? event.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    
+    if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46 ) {
       return false;
     }
     return true;
